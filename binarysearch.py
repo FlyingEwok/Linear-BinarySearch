bea782cea3ad24ca6cf9e0ff2e36844275a287d8
FlyingEwok <s254525@ocsb.local>
2020-10-20 13:57:33 -0400

    added a try catch

--- binarysearch.py
+++ binarysearch.py
@@ -2,23 +2,35 @@ def binarySearch (list, l, listLength, value):
     # Set up mid point variable
     if listLength >= l: 
   
-        mid = l + (listLength - l) // 2
+        midPoint = l + (listLength - l) // 2
   
-        if list[mid] == value: 
-            return mid 
-        elif list[mid] > value: 
-            return binarySearch(list, l, mid-1, value) 
-        else: 
-            return binarySearch(list, mid + 1, listLength, value) 
-    else: 
+        if list[midPoint] == value: # return the midpoint if the value is midpoint
+            return midPoint 
+        elif list[midPoint] > value: # Do a search to the left of midpoint for value then return that 
+            return binarySearch(list, l, midPoint-1, value) 
+        else: # Do a search to the right of midpoint for value then return that
+            return binarySearch(list, midPoint + 1, listLength, value) 
+    else: # if value not in list then return -1
         return -1
 
-searchList = [1, 2, 3, 4, 5, 6, 7, 8]
-value = int(input("Enter a number: "))
+searchList = [1, 2, 3, 4, 5, 6, 7, 8, 99]
 
-result = binarySearch(searchList, 0, len(searchList)-1, value)
+# Ask user for a number
+try:
+    value = int(input("Enter a number: "))
+    # perform the binary search on the search list
+    result = binarySearch(searchList, 0, len(searchList)-1, value)
 
-if result != -1: 
-    print ("Element is at index", result) 
-else: 
-    print ("Element is not in list") 
+    # Print the results in a statement
+    if result != -1: 
+        print ("Element is at index", result) 
+    else: 
+        print ("Element is not in list") 
+
+except ValueError:
+    print("That's no number! Try again.")    
+
+
+
+
+
